373fb6b4facc7db43ed2e972f91a32d83b399606
danilozabala <dczii@live.com>
2019-07-05 18:25:30 +0800

    Add swipeable UI.

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -5,16 +5,50 @@ import {
     ImageBackground,
     TouchableOpacity,
     Image,
+    ListView,
     Text
 } from 'react-native';
+import { List, ListItem, Button } from 'native-base';
 import _ from 'lodash';
 
 function RecipesList(props) {
     let { dataList } = props;
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
 
     return(
         <ScrollView style={{ flex: 1 }}>
-            {_.map(dataList, (data, idx) => {
+            <List
+                leftOpenValue={75}
+                rightOpenValue={-75}
+                dataSource={ds.cloneWithRows(dataList)}
+                renderRow={(data, idx) => {
+                    console.log(data)
+                    return (
+                    <ImageBackground 
+                        style={styles.background}
+                        source={{ uri: data.imageUrl }}>
+                        <ListItem style={{flex: 1, padding: 0, borderBottomWidth: 0, flexDirection: 'column', alignItems:'flex-start'}} key={idx} noIndent>
+                        <Text style={styles.title}>{data.title}</Text>
+                        <Text style={styles.desc}>{data.description}</Text>
+                        </ListItem>
+                    </ImageBackground>)
+                }}
+                renderLeftHiddenRow={(data, idx) =>
+                    <Button full  primary style={styles.btn} onPress={() => {
+                        props.onShowModal({openModal: true, modalView: 'update' })
+                        props.editData({id: idx, data})
+                    }}>
+                        <Image
+                            style={{width: 20, height: 20}}
+                            source={require('../../assets/images/edit-icon.png')}
+                            />
+                    </Button>}
+                  renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+                    <Button full danger>
+                    <Text>Qewr</Text>
+                    </Button>}
+            />
+            {/* {_.map(dataList, (data, idx) => {
                 return(
                     <ImageBackground key={idx} 
                         style={styles.background}
@@ -34,7 +68,7 @@ function RecipesList(props) {
                         </TouchableOpacity>
                     </ImageBackground>
                 );
-            })}
+            })} */}
         </ScrollView>
     );
 }
@@ -43,15 +77,6 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         padding: 10,
-        marginBottom: 10,
-    },
-    container: {
-      flex: 1,
-      height: 60,
-      borderWidth: 1,
-      justifyContent: 'center',
-      alignItems: 'flex-start',
-      paddingHorizontal: 10,
     },
     title: {
         fontSize: 16,
@@ -61,9 +86,6 @@ const styles = StyleSheet.create({
         fontSize: 12,
     },
     btn: {
-        position: 'absolute',
-        right: 20,
-        top: 15
     }
 });
 
